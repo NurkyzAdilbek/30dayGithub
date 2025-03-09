cec2d019459f3a23939809cb1aed2e824a45ed71
NurkyzAdilbek <nurkyz.adilbekova.kk@gmail.com>
2025-03-09 21:19:31 +0100

    commit

--- src/day15/Day15.java
+++ src/day15/Day15.java
@@ -9,22 +9,22 @@ public class Day15 {
     }
     public static int factorial(int n) {
         if (n == 0) {
-            return 1; // Базовый случай
+            return 1;
         }
-        return n * factorial(n - 1); // Рекурсивный вызов
+        return n * factorial(n - 1);
     }
     public static int fibonacci(int n) {
         if (n == 0) {
-            return 0; // Базовый случай
+            return 0;
         } else if (n == 1) {
-            return 1; // Базовый случай
+            return 1;
         }
-        return fibonacci(n - 1) + fibonacci(n - 2); // Рекурсивный вызов
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
     public static int sumDigits(int n) {
         if (n == 0) {
-            return 0; // Базовый случай
+            return 0;
         }
-        return n % 10 + sumDigits(n / 10); // Рекурсивный вызов
+        return n % 10 + sumDigits(n / 10);
     }
 }
